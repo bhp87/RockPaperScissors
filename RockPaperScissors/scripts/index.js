0bec0edcc1a062670c1894be19773d2f225fee0b
bhp <vovafc87@gmail.com>
2023-12-07 14:50:28 +0200

    added wins counter

--- RockPaperScissors/scripts/index.js
+++ RockPaperScissors/scripts/index.js
@@ -1,11 +1,14 @@
 const choices = ['rock', 'paper', 'scissors'];
+let PLAYER_WINS_COUNT = 0;
+let COMPUTER_WINS_COUNT = 0;
+
 const getComputerChoice = () => {
 
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 const getUserChoice = () => {
-    const userChoice= prompt("Enter your choice (rock, paper, or scissors):");
+    const userChoice = prompt("Enter your choice (rock, paper, or scissors):");
     return userChoice;
 }
 
@@ -26,14 +29,20 @@ const playRound = (playerSelection, computerSelection) => {
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
+        ++PLAYER_WINS_COUNT;
         return "You Win! " + playerSelection + " beats " + computerSelection + ".";
     } else {
+        ++COMPUTER_WINS_COUNT;
         return "You Lose! " + computerSelection + " beats " + playerSelection + ".";
     }
+
 }
 const game = () => {
-
-    console.log(playRound(getUserChoice(), getComputerChoice()));
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(getUserChoice(), getComputerChoice()));
+        console.log("Player wins : " + PLAYER_WINS_COUNT);
+        console.log("Computer wins : " + COMPUTER_WINS_COUNT);
+    }
 }
 
 game();
